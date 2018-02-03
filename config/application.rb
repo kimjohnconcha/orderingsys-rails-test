c39391aa39f706176d38bfe942892d7f51bd035f
kimjohnconcha <kimjohnconcha@icloud.com>
2018-02-03 14:00:33 +0800

    gem update

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module EcommerceTest
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.assets.initialize_on_precompile = false
+
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Settings in config/environments/* take precedence over those specified here.
