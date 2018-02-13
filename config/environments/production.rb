48547c14ad1106a9465b34046b0ba7f66836e1ff
kimjohnconcha <kimjohnconcha@icloud.com>
2018-02-13 22:36:20 +0800

    asst fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -105,5 +105,5 @@ Rails.application.configure do
    :enable_starttls_auto => true
   }
 
-  config.action_mailer.default_url_options = { :host => 'https://orderingsystemconcha.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'http://orderingsystemconcha.herokuapp.com'}
 end
